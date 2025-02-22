13583abd889d7af030b4e543dba366953f996739
Charley Crowson <charleycrowson1997@gmail.com>
2025-02-22 13:12:17 +0000

    Add MongoDB Project Model

---
+++ database/models/project.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+interface Project {
+  title: string;
+  description: string;
+  tech?: string[];
+  github?: string;
+  live?: string;
+  image?: string;
+  created_at: Date;
+  updated_at?: Date;
+  status: string;
+  stage: string;
+}
+
+const schema = new Schema<Project>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  tech: { type: [String], required: false },
+  github: { type: String, required: false },
+  live: { type: String, required: false },
+  image: { type: String, required: false },
+  created_at: { type: Date, required: true },
+  updated_at: { type: Date, required: false },
+  status: { type: String, required: true },
+  stage: { type: String, required: true },
+});
+
+export const projectModel = mongoose.model<Project>("Project", schema);
