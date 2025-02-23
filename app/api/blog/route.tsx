21f4bf1e020a52d3d28ad01b3e799e484501265a
Charley Crowson <charleycrowson1997@gmail.com>
2025-02-23 15:12:24 +0000

    Create and test endpoint for GET /api/blog, used to retrieve all blog posts. (TDD)

---
+++ app/api/blog/route.tsx
@@ -0,0 +1,16 @@
+import { connect } from "../../../database/connection";
+import { NextResponse } from "next/server";
+import postModel from "../../../database/models/post";
+
+export async function GET(): Promise<NextResponse> {
+  try {
+    await connect();
+    const posts = await postModel.find({});
+    return NextResponse.json({ success: true, data: posts }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: `Error fetching posts : ${(error as Error).message}` },
+      { status: 500 }
+    );
+  }
+}
