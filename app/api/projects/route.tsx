917b55d51fdc941a39a606c274fdc12adfc0f236
Charley Crowson <charleycrowson1997@gmail.com>
2025-02-24 14:50:26 +0000

    Create and test POST /api/projects, insert a new project into db

--- app/api/projects/route.tsx
+++ app/api/projects/route.tsx
@@ -1,5 +1,5 @@
 import { connect } from "../../../database/connection";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import projectModel from "../../../database/models/project";
 
 export async function GET(): Promise<NextResponse> {
@@ -10,6 +10,22 @@ export async function GET(): Promise<NextResponse> {
       return NextResponse.json({ success: false, message: "No projects found." }, { status: 404 });
     }
     return NextResponse.json({ success: true, data: projects }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ success: false, message: (error as Error).message }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  try {
+    await connect();
+    const body = await request.json();
+    body.created_at = Date.now();
+    const project = new projectModel(body);
+
+    await project.validate();
+    await project.save();
+
+    return NextResponse.json({ success: true, data: project }, { status: 201 });
   } catch (error) {
     return NextResponse.json({ success: false, message: (error as Error).message }, { status: 500 });
   }
